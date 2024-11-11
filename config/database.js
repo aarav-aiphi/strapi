287e7992bac403b7d8e322929495080a8b76b63a
aarav-aiphi <aarav.v@aiphi.ai>
2024-11-12 00:26:11 +0530

    strapi

---
+++ config/database.js
@@ -0,0 +1,27 @@
+// strapi/config/database.js
+
+const path = require('path');
+
+module.exports = ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'postgres');
+
+  const connections = {
+    postgres: {
+      connection: {
+        connectionString: env('DATABASE_URL'),
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      pool: { min: 2, max: 10 },
+    },
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    },
+  };
+};
